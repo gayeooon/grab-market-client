2430ddfafb9c8cccf0a7185a8932754583f17039
가연 <68602093+gayeooon@users.noreply.github.com>
2023-11-22 22:43:33 +0900

    Link tag applied

--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./index.css";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 function MainPageComponent() {
   const [products, setProducts] = React.useState([]);
@@ -22,32 +23,39 @@ function MainPageComponent() {
     <div>
       <div id="header">
         <div id="header-area">
-          <img src="images/icons/logo.png" />
+          <img src="images/icons/logo.png" alt="" />
         </div>
       </div>
       <div id="body">
         <div id="banner">
-          <img src="images/banners/banner1.png" />
+          <img src="images/banners/banner1.png" alt="" />
         </div>
         <h1>Selling Products</h1>
         <div id="product-list">
           {products.map(function (product, index) {
             return (
               <div className="product-card">
-                <div>
-                  <img className="product-img" src={product.imageUrl} />
-                </div>
-                <div className="product-contents">
-                  <span className="product-name">{product.name}</span>
-                  <span className="product-price">{product.price}won</span>
-                  <div className="product-seller">
+                <Link className="product-link" to={`/products/${index}`}>
+                  <div>
                     <img
-                      className="product-avatar"
-                      src="images/icons/avatar.png"
+                      className="product-img"
+                      src={product.imageUrl}
+                      alt=""
                     />
-                    <span>{product.seller}}</span>
                   </div>
-                </div>
+                  <div className="product-contents">
+                    <span className="product-name">{product.name}</span>
+                    <span className="product-price">{product.price}won</span>
+                    <div className="product-seller">
+                      <img
+                        className="product-avatar"
+                        src="images/icons/avatar.png"
+                        alt=""
+                      />
+                      <span>{product.seller}</span>
+                    </div>
+                  </div>
+                </Link>
               </div>
             );
           })}
